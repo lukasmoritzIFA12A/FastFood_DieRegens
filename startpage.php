acc1e32f9b3e67881ecb418aac43939a5ca9fbfc
Tizi <Tizian.Fiorentino@bs-erlangen.de>
2025-02-10 12:20:05 +0100

    Für Max Baby

---
+++ startpage.php
@@ -0,0 +1,17 @@
+<?php
+$welcomeMessage = "Willkommen auf meiner PHP-Seite!";
+?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fixierte Logos mit PHP</title>
+    <link rel="stylesheet" href="style.css">  <!-- Verlinkung der externen CSS-Datei -->
+
+    <div class="box">
+<!--        <h1>--><?php //echo $welcomeMessage; ?><!--</h1>-->
+<!--        <p>Das Logo bleibt jetzt fixiert und verändert seine Größe nicht mehr.</p>-->
+
+        <button class="btn-custom">Klick mich!</button>
+    </div>
